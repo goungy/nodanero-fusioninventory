31b4c93a77e2235d0b4667301f8a5f5ba6da1fef
Joaquin Fernandez Llamas <nodanero@gmail.com>
2017-02-28 21:06:56 +0700

    Modified the params call
    Modified the parameters call in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,20 +2,24 @@
 # Configures fusioninventory-agent in host.
 #
 
-class fusioninventory (Boolean $cronscript_enable = false,
-  String $glpiserverurl  = 'http:///',
-  Boolean $no_proxy = true,
-  Boolean $service_enable = true,
-  )
-{
-  include
-  'fusioninventory::install'
+class fusioninventory (
+  $pkgfusion          = $fusioninventory::params::pkgfusion,
+  $glpiserverurl      = $fusioninventory::params::glpiserverurl,
+  $windowsmsi         = $fusioninventory::params::windowsmsi,
+  $crondest           = $fusioninventory::params::crondest,
+  $version            = $fusioninventory::params::version,
+  $Service_ensure     = $fusioninventory::params::ensure,
+  $service_enable     = $fusioninventory::params::enable,
+  $cronscript_enable  = $fusioninventory::params::cronscript_enable
+) inherits fusioninventory::params {
+    include
+    'fusioninventory::install'
 
   if ($cronscript_enable == true){
     include   'fusioninventory::cronscript'
   }
 
-  if ($service_enable == true){
+  if ($enable == true){
     include   'fusioninventory::service'
   }
 
